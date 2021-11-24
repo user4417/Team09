7ddb3b9f99a19a0f29ee1716a9f1c50b37ae1a8f
user4417 <d1084182102@gm.lhu.edu.tw>
2021-11-24 17:34:36 +0800

    no message

--- database/seeds/ClassesTableSeeder.php
+++ database/seeds/ClassesTableSeeder.php
@@ -1,19 +1,103 @@
 <?php
 
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
-class ClassesTableSeeder extends Seeder
+class TeamsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
      * @return void
      */
+    public function generateRandomString($length = 10) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    public function generateRandomName() {
+        $name = [
+            '鐵匠',
+            '木工',
+            '律師',
+            '圖書管理員',
+            '納粹黨衛軍',
+            '元素騎士',
+            '主席',
+            '心理學家',
+            '網路管理員',
+            '學生',
+            '表格撰寫者',
+            '會計師',
+            '警官',
+            '超商店員',
+            '程式作業員',
+        ];
+        return $name[rand(0, count($name)-1)];
+    }
+    public function generateRandomSp() {
+        $sp = ['打鐵',
+        '鋸木',
+        '辯護',
+        '法術',
+        '做肥皂',
+        '自殺',
+        '發表政件',
+        '分析心理',
+        '買乖乖',
+        '嘴砲',
+        '寫Excel',
+        '處理金錢',
+        '掃黑',
+        '計算該找多少',
+        '寫程式',
+        ];
+        return $Sp[rand(0, count($Sp)-1)];
+    }
+    public function generateRandomCity() {
+        $name = [
+            '鐵匠',
+            '木工',
+            '律師',
+            '圖書管理員',
+            '納粹黨衛軍',
+            '元素騎士',
+            '主席',
+            '心理學家',
+            '網路管理員',
+            '學生',
+            '表格撰寫者',
+            '會計師',
+            '警官',
+            '超商店員',
+            '程式作業員',
+        ];
+        return $name[rand(0, count($name)-1)];
+    }
     public function run()
     {
+        for ($i=0; $i<25; $i++) {
+            $name = $this->generateRandomName();
+            $city = $this->generateRandomCity();
+            $sp = $this->generateRandomSp();
+            $home = $city . "球場";
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
 
-        // $this->call(UsersTableSeeder::class);
+            DB::table('teams')->insert([
+                'name' => $name,
+                'zone' => $zone,
+                'city' => $city,
+                'home' => $home,
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+        }
     }
 }
-?>
--- database/seeds/VillagersTableSeeder.php
+++ database/seeds/VillagersTableSeeder.php
@@ -1,21 +1,67 @@
 <?php
 
+namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
-class VillagersTableSeeder extends Seeder
+class PlayersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
      * @return void
      */
+    public function generateRandomString($length = 10) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    public function generateRandomName() {
+        $first_name = $this->generateRandomString(rand(2, 15));
+        $first_name = strtolower($first_name);
+        $first_name = ucfirst($first_name);
+        $last_name = $this->generateRandomString(rand(2, 15));
+        $last_name = strtolower($last_name);
+        $last_name = ucfirst($last_name);
+        $name = $first_name . " ". $last_name;
+        return $name;
+    }
+    public function generateRandomGender() {
+        $positions = ['人妖','男','女','男男女女'];
+        return $positions[rand(0, count($positions)-1)];
+
+    }
+
+    public function generateRandomPlus() {
+        $positions = ['無改造', '小幅度', '一般', '大幅度'];
+        return $positions[rand(0, count($positions)-1)];
+
+    }
     public function run()
     {
-        //DB::table()->insert([
-        //   'name'
-        //])
-        // $this->call(UsersTableSeeder::class);
+        for ($i=0; $i<500; $i++)
+        {
+            $name = $this->generateRandomName();
+            $gender = $this->generateRandomGender();
+            $plus = $this->generateRandomPlus();
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+            DB::table('Villagers')->insert([
+                'name' => $name,
+                'cid' => rand(1, 25),
+                'gender' => $gender,
+                'press' => rand(1,100),
+                'plus' => $plus,
+                'monster' => rand(1, 100),
+                'Lead' => rand(1, 100),
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+        }
     }
 }
-?>
